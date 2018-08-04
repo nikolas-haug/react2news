87732dc61dd98984f2a896218dac85cfa5c670cc
Nikolas  Haug <nikolashaug@Nikolass-MacBook-Air.local>
2018-08-03 23:22:41 -0500

    add mongodb uri to server

--- server.js
+++ server.js
@@ -24,14 +24,23 @@ db.once("open", function() {
 });
 
 // Set up promises with mongoose
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
+// // Connect to the Mongo DB
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/nytreact",
+//   {
+//     useMongoClient: true
+//   }
+// );
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/nytreact",
-  {
-    useMongoClient: true
-  }
-);
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
 
 // Start the API server
 app.listen(PORT, function() {
